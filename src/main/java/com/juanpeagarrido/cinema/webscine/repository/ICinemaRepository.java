f775912cc4963728c2b8d45c9a6e1ba44cdcf824
Juan Peña <juanpeagarrido@gmail.com>
2022-04-13 17:07:06 +0200

    Cambios en la parte del Servidor

--- src/main/java/com/juanpeagarrido/cinema/webscine/repository/ICinemaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.juanpeagarrido.cinema.webscine.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.juanpeagarrido.cinema.webscine.entities.Pelicula;
-
-public interface ICinemaRepository extends JpaRepository<Pelicula, Integer>{
-
-}
